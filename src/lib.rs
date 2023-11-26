ad8b54ebff9925500fcfe83c5d427bcff59e94e6
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-11-26 12:17:50 +1000

    Add docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,34 @@
 use ibig::{ibig, IBig};
 
+/// Trait ExtGcd is used to define the extended Euclidean algorithm
+/// method ext_gcd for data type IBig
 pub trait ExtGcd {
+    /// Find the greatest common divisor (gcd)
+    /// between the two numbers self and rhs & return it
+    /// alongside the coefficients of Bézout's identity.
+    ///
+    /// # Arguments
+    ///
+    /// * `rhs` - An IBig integer  
+    ///
+    /// # Returns
+    ///
+    /// * `(IBig, IBig, IBig)` - A tuple containing the gcd & coefficients of Bézout's identity.
     fn ext_gcd(self, rhs: IBig) -> (IBig, IBig, IBig);
 }
 
 impl ExtGcd for IBig {
+    /// Realize the extended Euclidean algorithm to find
+    /// the greatest common divisor (gcd) between self and y & return it
+    /// alongside the coefficients of Bézout's identity.
+    ///
+    /// # Arguments
+    ///
+    /// * `y` - An IBig integer  
+    ///
+    /// # Returns
+    ///
+    /// * `(IBig, IBig, IBig)` - A tuple containing gcd and coefficients of Bézout's identity.
     fn ext_gcd(mut self, mut y: IBig) -> (IBig, IBig, IBig) {
         let mut a1 = ibig!(0);
         let mut b2 = ibig!(0);
